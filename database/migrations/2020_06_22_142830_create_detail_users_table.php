45723771cebfdb911eaae2b7e3631c7b6cb9bf34
marco-silvestri <m.silvestri03@gmail.com>
2020-06-22 17:41:10 +0200

    Base structure

---
+++ database/migrations/2020_06_22_142830_create_detail_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_users', function (Blueprint $table) {
+            
+            $table->unsignedBigInt('user_id');
+            $table->string('address');
+            $table->string('phone');
+
+            //Create the relation
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_users');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('home', 'HomeController@index')->name('home');
+Route::resource('users', 'UserController');
+Route::resource('details', 'DetailUserController');
+Route::resource('posts', 'PostController');
